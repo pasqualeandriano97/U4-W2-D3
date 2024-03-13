929de9b755b76357bf2e9c87bc1f5d316b2bf785
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-03-13 17:25:57 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         products1.add(new Product("Book5", "Books", 1100));
         products1.add(new Product("Boy5", "Boys", 1100));
         products1.add(new Product("Baby8", "Baby", 110));
-        System.out.println(products.stream().filter(p -> p.getCategory().equals("Books")&&(p.getPrice()>100)).toList());
+        System.out.println("Prodotti di categoria Books con prezzo minore di 100: "+products.stream().filter(p -> p.getCategory().equals("Books")&&(p.getPrice()>100)).toList());
         List<Product> filteredProducts= products.stream().filter(p -> p.getCategory().equals("Boys")).toList();
         Customer customer = new Customer("John", 1);
         Customer customer1 = new Customer("Jane", 2);
@@ -33,16 +33,14 @@ public class Main {
         orders.add(order2);
 
       filteredProducts.stream().forEach(p -> p.setPrice(p.getPrice()*0.9));
-      System.out.println(filteredProducts);
-        System.out.println(order);
-        System.out.println(order1);
-        System.out.println(order2);
+      System.out.println("Prodotti con categoria Boys e sconto del 10% : "+filteredProducts);
+
         System.out.println("Ordini totali"+orders);
        orders.stream().forEach(o -> o.getProducts().forEach(product -> System.out.println(product.sameCategoryB(o.getProducts()))));
 
         for(Order o:orders) {
             if (o.getOrderDate().isAfter(LocalDate.of(2021, 2, 1))&&o.getOrderDate().isBefore(LocalDate.of(2021, 4, 1))) {
-                System.out.println(o);
+                System.out.println("Ordine effettuato nelle date limite: "+o);
             }
         }
     }
